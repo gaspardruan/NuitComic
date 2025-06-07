d364ad89ed9a54e7372eb723321ee5ae741fa348
GaspardRuan <1039553124@qq.com>
2025-06-07 23:48:44 +0800

    fix: ComicReader header back title

--- app/comic/[id]/view.tsx
+++ app/comic/[id]/view.tsx
@@ -1,10 +1,13 @@
 import { getComicAllChapter } from "@/axios/comic";
+import { tintColorLight } from "@/common/theme";
 import { shortTitle } from "@/common/util";
 import ComicReader from "@/components/comic/view/ComicReader";
+import ThemedText from "@/components/ThemedText";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { useQuery } from "@tanstack/react-query";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { View } from "react-native";
 
 export default function ComicView() {
   const { id, index } = useLocalSearchParams<{ id: string; index: string }>();
@@ -46,13 +49,20 @@ export default function ComicView() {
       <Stack.Screen
         options={{
           headerShown: showHeader,
-          headerBackTitle: shortTitle(title),
           title: "",
           headerBackTitleStyle: { fontSize: 16 },
           headerTransparent: true,
           headerStyle: {
             backgroundColor: backgroundColor,
           },
+          headerBackVisible: true,
+          headerLeft: () => (
+            <View style={{ marginLeft: -17 }}>
+              <ThemedText type="emphasis">{shortTitle(title)}</ThemedText>
+            </View>
+          ),
+          headerRight: () => null,
+          headerTintColor: tintColorLight,
         }}
       />
       <ComicReader
